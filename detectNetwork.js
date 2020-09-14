d54ff8f0fbbc79aad8ed9144b443646c829b265c
Thomas Brannan <th.c.brannan@gmail.com>
2020-09-13 20:18:48 -0700

    In detectNetwork.js: made detectNetwork return the correct network for every prefix and length combination for Diner's Club and American Express

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,8 +1,10 @@
-// Given a credit card number, this function should return a string with the 
+/* eslint-disable comma-dangle */
+/* eslint-disable no-var */
+// Given a credit card number, this function should return a string with the
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy! 
+// How can you tell one card network from another? Easy!
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
@@ -13,7 +15,24 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+  var data = {
+    'American Express': {
+      prefixes: [34, 37], lengths: [15]
+    },
+    'Diner\'s Club': {
+      prefixes: [38, 39], lengths: [14]
+    }
+  }
+
+  var twoDigitPrefix = Number(cardNumber.toString().slice(0, 2));
+  var length = cardNumber.toString().length;
+
+  for (var network in data) {
+    if (data[network].prefixes.includes(twoDigitPrefix) && data[network].lengths.includes(length)) {
+      return network;
+    }
+  }
+
+  return undefined;
 };
-
-
-
