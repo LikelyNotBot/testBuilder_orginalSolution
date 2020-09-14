b51991d12ee6b8c91b3e0f49239a69692b6411ce
Thomas Brannan <th.c.brannan@gmail.com>
2020-09-13 21:40:12 -0700

    In detectNetwork.js: reworked the function to be more friendly to networks with a prefix of a length other than 2. It will now work with networks that have prefixes of any length, and with networks that have different prefixes with different lengths.

--- detectNetwork.js
+++ detectNetwork.js
@@ -14,26 +14,51 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+  // Visa always has a prefix of 4 and a length of 13, 16, or 19.
+  // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 
   var data = {
     'American Express': {
-      prefixes: [34, 37], lengths: [15]
+      prefixes: ['34', '37'], lengths: [15]
     },
     'Diner\'s Club': {
-      prefixes: [38, 39], lengths: [14]
+      prefixes: ['38', '39'], lengths: [14]
+    },
+    'Visa': {
+    	prefixes: ['4'], lengths: [13, 16, 19]
+    },
+    'MasterCard': {
+    	prefixes: ['51', '52', '53', '54', '55'], lengths: [16]
     }
   }
 
-  var twoDigitPrefix = Number(cardNumber.toString().slice(0, 2));
-  var length = cardNumber.toString().length;
-
-  for (var network in data) {
-    if (data[network].prefixes.includes(twoDigitPrefix) && data[network].lengths.includes(length)) {
-      return network;
+for (var network in data) {
+    if (data[network].lengths.includes(cardNumber.length)) {
+      var prefixes = data[network].prefixes;
+      for (var i = 0; i < prefixes.length; i++) {
+        if (prefixes[i] === cardNumber.slice(0, prefixes[i].length)) {
+          return network;
+        }
+      }
     }
   }
 
   return undefined;
 };
+
+/**
+console.log(detectNetwork('38345678901234')); 	// (Diner's Club)
+console.log(detectNetwork('39345678901234')); 	//  (Diner's Club)
+console.log(detectNetwork('343456789012345')); 	//  (American Express)
+console.log(detectNetwork('373456789012345')); 	//  (American Express)
+console.log(detectNetwork('4123456789012')); 	//  (Visa)
+console.log(detectNetwork('4123456789012345')); //  (Visa)
+console.log(detectNetwork('4123456789012345678')); //  (Visa)
+console.log(detectNetwork('5112345678901234')); //  (MasterCard)
+console.log(detectNetwork('5212345678901234')); //  (MasterCard)
+console.log(detectNetwork('5312345678901234')); //  (MasterCard)
+console.log(detectNetwork('5412345678901234')); //  (MasterCard)
+console.log(detectNetwork('5512345678901234')); //  (MasterCard)
+ */
