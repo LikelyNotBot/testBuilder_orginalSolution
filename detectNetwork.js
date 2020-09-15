30c633f3e0287496135f615e6b528e36b65bb376
Thomas Brannan <th.c.brannan@gmail.com>
2020-09-14 21:17:30 -0700

    In detectNetwork.js:   deleted excessive comments which had accumulated

--- detectNetwork.js
+++ detectNetwork.js
@@ -5,22 +5,8 @@
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy!
-// There are two indicators:
-//   1. The first few numbers (called the prefix)
-//   2. The number of digits in the number (called the length)
 
 var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
-  // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
-  // The American Express network always starts with a 34 or 37 and is 15 digits long
-  // Visa always has a prefix of 4 and a length of 13, 16, or 19.
-  // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
-  // Discover always has a prefix of 6011, 644-649, or 65, and a length of 16 or 19.
-  // Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.
-  // China UnionPay always has a prefix of 622126-622925, 624-626, or 6282-6288 and a length of 16-19.
-  // Switch always has a prefix of 4903, 4905, 4911, 4936, 564182, 633110, 6333, or 6759 and a length of 16, 18, or 19.
-
   // For type safety, this is convient. Some of my tests do pass numbers.
   cardNumber = cardNumber.toString();
 
@@ -89,18 +75,6 @@ var detectNetwork = function(cardNumber) {
       }
       return longestMatch[0];
   }
-
-/*
-  } else {
-    var longestMatch = candidates[0][1];
-    for (var i=0; i < candidates[0].prefixes.length; i++) {
-      if (longestMatch[1] < candidates[i][1]) {
-        longestMatch = candidates[i];
-      }
-    }
-    return longestMatch[0];
-  }
-*/
 };
 
 /**
@@ -108,23 +82,6 @@ var detectNetwork = function(cardNumber) {
  *  as arrays of length 2 or 1 each.
  * @returns [array] prefixes
  */
-/**
-function getPrefixesFromRanges(ranges) {
-  var prefixes =
-  _.chain(ranges)
-    .map(function(range) {
-    	if (range.length === 1) {
-    		return range[0];
-    	} else if (range.length === 2) {
-    		return _.range(range[0], range[1]+1);
-    	}
-    })
-    .flatten()
-    .map(function(prefix) {return prefix.toString();})
-    .value();
-  return prefixes;
-}
-*/
 function getPrefixesFromRanges(ranges) {
   // for every range in ranges, create an array
   // with all the numbers in that range.
@@ -139,18 +96,3 @@ function getPrefixesFromRanges(ranges) {
     return res;
   }).flat();
 }
-
-/**
-console.log(detectNetwork('38345678901234')); 	// (Diner's Club)
-console.log(detectNetwork('39345678901234')); 	//  (Diner's Club)
-console.log(detectNetwork('343456789012345')); 	//  (American Express)
-console.log(detectNetwork('373456789012345')); 	//  (American Express)
-console.log(detectNetwork('4123456789012')); 	//  (Visa)
-console.log(detectNetwork('4123456789012345')); //  (Visa)
-console.log(detectNetwork('4123456789012345678')); //  (Visa)
-console.log(detectNetwork('5112345678901234')); //  (MasterCard)
-console.log(detectNetwork('5212345678901234')); //  (MasterCard)
-console.log(detectNetwork('5312345678901234')); //  (MasterCard)
-console.log(detectNetwork('5412345678901234')); //  (MasterCard)
-console.log(detectNetwork('5512345678901234')); //  (MasterCard)
- */
